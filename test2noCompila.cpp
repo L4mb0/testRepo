119c003cfbb773e6c0a92ec5644978c9a134c256
L4mb0 <juan.navarro@utec.edu.pe>
2019-03-28 12:27:27 -0500

    no compila

---
+++ test2noCompila.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <map>
+#include <set>
+#include <limits>
+#include <queue>
+#include <cmath>
+#include <algorithm>
+#include<bits/stdc++.h>
+#include <future>
+#include <array>
+#define FOR(m) for(int i=0;i<m;i++)
+#define ll long long int
+#define scl1(a)             scanf("%lld", &a)
+#define REP(i,n) FOR(i,0,n)
+#define pb(x) push_back(x)
+#define MAX MAX_INT
+#define fst first
+#define snd second
+#define foritpnt for(auto it=datos->begin(); it != datos->end(); it++)
+#define forit for(auto it=datos.begin(); it != datos.end(); it++)
+#define sz(x) int(x.size())
+#define all(v) v.begin(),v.end()
+#define mp(x,y) make_pair(x,y)
+#define fastcin std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+using namespace std;
+
+int main(){
+    fastcin
+
+    int counter=0;
+    char txt[1000], temp;
+
+    ci
+    }
+
+    cout<<counter;
+
+
+    return 0;
+}
